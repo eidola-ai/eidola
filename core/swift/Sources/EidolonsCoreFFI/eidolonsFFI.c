51011db3a0966c425d2ca446106be87fc1cc9a00
Mike Marcacci <mike.marcacci@gmail.com>
2025-12-17 10:34:00 -0800

    Get a basic SwiftUI app compiling on macOS and iOS

---
+++ core/swift/Sources/EidolonsCoreFFI/eidolonsFFI.c
@@ -0,0 +1,4 @@
+// This file exists so Swift Package Manager has something to compile for the eidolonsFFI module.
+// The actual implementation is in the XCFramework (libeidolons.a).
+// This module just exposes the C header interface to Swift.
+#include "eidolonsFFI.h"
