19f3155f72a38262915d2734b8925cf88134d109
Mike Marcacci <mike.marcacci@gmail.com>
2025-12-17 17:31:17 -0800

    Add mkServer

--- flake.nix
+++ flake.nix
@@ -87,40 +87,100 @@
           RUSTFLAGS = "-C debuginfo=0 -C target-cpu=generic";
         };
 
-        mkSystemPackages = system: {
-          server = mkServer system;
-          server-oci = mkServerOCI system;
-          core = mkCore system;
+        # Build the server binary for a target system
+        mkServer =
+          targetSystem:
+          let
+            isNative = targetSystem == system;
+
+            # Map system identifier to pkgsCross attribute name
+            # Native builds use pkgs directly; cross builds need pkgsCross
+            crossPkgsAttr = {
+              "aarch64-darwin" = "aarch64-darwin";
+              "x86_64-darwin" = "x86_64-darwin";
+              "aarch64-linux" = "aarch64-multiplatform-musl"; # musl for static linking
+              "x86_64-linux" = "musl64"; # musl for static linking
+            }.${targetSystem} or (throw "Unknown target system: ${targetSystem}");
+
+            targetPkgs = if isNative then pkgs else pkgs.pkgsCross.${crossPkgsAttr};
+
+            # Map system identifier to Rust target triple
+            rustTarget = {
+              "aarch64-darwin" = "aarch64-apple-darwin";
+              "x86_64-darwin" = "x86_64-apple-darwin";
+              "aarch64-linux" = "aarch64-unknown-linux-musl";
+              "x86_64-linux" = "x86_64-unknown-linux-musl";
+            }.${targetSystem} or (throw "Unknown target system: ${targetSystem}");
+
+            # Crane uses target pkgs (for linker/libc) but host toolchain (for cargo)
+            craneLibTarget = (crane.mkLib targetPkgs).overrideToolchain (_: rustToolchain);
+
+            # Cross-compilation needs CARGO_BUILD_TARGET set
+            targetArgs =
+              if isNative then
+                { }
+              else
+                { CARGO_BUILD_TARGET = rustTarget; };
+
+            serverArtifacts = craneLibTarget.buildDepsOnly (
+              commonArgs
+              // targetArgs
+              // {
+                pname = "eidolons-server-deps-${targetSystem}";
+                cargoExtraArgs = "--package eidolons-server";
+              }
+            );
+          in
+          craneLibTarget.buildPackage (
+            commonArgs
+            // targetArgs
+            // {
+              cargoArtifacts = serverArtifacts;
+              pname = "eidolons-server-${targetSystem}";
+              cargoExtraArgs = "--bin eidolons-server";
+            }
+          );
+
+        mkSystemPackages = targetSystem: {
+          server = mkServer targetSystem;
+          # server-oci = mkServerOCI targetPkgs;
+          # core = mkCore targetPkgs;
         };
 
       in
       {
         # Default packages for current system
-        packages = {
+        packages = mkSystemPackages system // {
           # Packages built using the native system.
-          inherit mkSystemPackages system;
-          cors-swift-bindings = mkCoresSwiftBindings;
-          cors-swift-xcframework = mkCoresSwiftXCFramework system; # depends on all cross-compiled apple targets
+          # cors-swift-bindings = mkCoresSwiftBindings;
+          # cors-swift-xcframework = mkCoresSwiftXCFramework system; # depends on all cross-compiled apple targets
 
-          cross = builtins.listToAttrs (
-            map
-              (t: {
-                name = t;
-                value = mkSystemPackages pkgs.pkgsCross.${t};
-              })
-              [
-                # Linux targets
-                "aarch64-linux"
-                "x86_64-linux"
+          # https://nix.dev/tutorials/cross-compilation.html#cross-compilation
+          # cross = builtins.listToAttrs (
+          #   map
+          #     (t: {
+          #       name = t;
+          #       value = mkSystemPackages pkgs.pkgsCross.${t};
+          #     })
+          #     [
+          #       # Linux targets
+          #       # "aarch64-linux"
+          #       # "x86_64-linux"
 
-                # Apple targets
-                "aarch64-apple-darwin"
-                "x86_64-apple-darwin"
-                "aarch64-apple-ios"
-                "aarch64-apple-ios-sim"
-                "x86_64-apple-ios"
-              ]
-          );
+          #       # Apple targets
+          #       # "aarch64-apple-darwin"
+          #       # "x86_64-apple-darwin"
+          #       # "aarch64-apple-ios"
+          #       # "aarch64-apple-ios-sim"
+          #       # "x86_64-apple-ios"
+          #       #
+          #       #
+          #       "aarch64-darwin"
+          #       "x86_64-darwin"
+          #       "iphone64"
+          #       "iphone64-simulator"
+          #     ]
+          # );
         };
 
         # Development shell with Rust toolchain and tools
